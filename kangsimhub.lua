ea4970357bd3aab007e4f09b4e0ef24185f388b6
adrstone <lethanhnamnd2021@gmail.com>
2025-04-21 20:10:46 +0800

    Add files via upload

---
+++ kangsimhub.lua
@@ -0,0 +1,15 @@
+-- Key hợp lệ
+local validKey = 'caa72c81673275da083638d9'
+
+-- Giả sử bạn đã nhận key từ người dùng ở đâu đó, gán vào getgenv().Key:
+getgenv().Key = 'caa72c81673275da083638d9'  -- <-- Đặt đúng key ở đây thì phần bên trong if sẽ chạy
+
+-- Kiểm tra và thực thi
+if getgenv().Key == validKey then
+    -- Khi đúng key, phần này sẽ chạy
+    print('Key hợp lệ, script bắt đầu thực thi.')
+    -- Ví dụ chạy script từ URL:
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/adrstone/adu/refs/heads/main/kangsim.lua"))()
+else
+    game.Players.LocalPlayer:Kick('Invalid key! Please enter the correct key.')
+end
