c23ae71ddebc8fc037e8e99df13587bbe1bc7416
adrstone <lethanhnamnd2021@gmail.com>
2025-04-21 20:18:39 +0800

    Update kangsimhub.lua

--- kangsimhub.lua
+++ kangsimhub.lua
@@ -2,7 +2,7 @@
 local validKey = 'caa72c81673275da083638d9'
 
 -- Giả sử bạn đã nhận key từ người dùng ở đâu đó, gán vào getgenv().Key:
-getgenv().Key = 'caa72c81673275da083638d9'  -- <-- Đặt đúng key ở đây thì phần bên trong if sẽ chạy
+getgenv().Key = ''  -- <-- Đặt đúng key ở đây thì phần bên trong if sẽ chạy
 
 -- Kiểm tra và thực thi
 if getgenv().Key == validKey then
